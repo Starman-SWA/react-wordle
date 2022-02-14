67290c2969d03b028bd2dfc63f2ca41c8d638a41
jameschen-swa <zhengchenprc@outlook.com>
2022-02-14 17:55:41 +0800

    modify share text index

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -8,7 +8,7 @@ export const shareStatus = (
   isHardMode: boolean
 ) => {
   navigator.clipboard.writeText(
-    `${GAME_TITLE} ${solutionIndex} ${lost ? 'X' : guesses.length}/6${
+    `${GAME_TITLE} ${solutionIndex+1} ${lost ? 'X' : guesses.length}/6${
       isHardMode ? '*' : ''
     }\n\n` + generateEmojiGrid(guesses) + `\n\n${GAME_LINK}`
   )
